09667d4dcefe0fb165819900275bdfa401fa6470
xitmen <xitmen@163.com>
2018-11-29 11:20:15 +0800

    屏蔽环迅推荐码

--- src/components/visiting-card/visiting-card.vue
+++ src/components/visiting-card/visiting-card.vue
@@ -7,8 +7,8 @@
           <div id="qrcode"></div>
           <img src="/static/img/logo.jpg" />
         </div>
-        <span class=code v-show="false"><em>盒粉推荐码:</em>{{code}}</span>
-        <span class="hx_code" v-show="hx_code"><em>环迅推荐码:</em>{{hx_code}}</span>
+        <span class=code v-show="code"><em>盒粉推荐码:</em>{{code}}</span>
+        <span class="hx_code" v-show="false"><em>环迅推荐码:</em>{{hx_code}}</span>
       </div>
       <p class="share-btn">
         <a href="javascript:;" class="btn" @click="shareHandler">{{text}}</a>
